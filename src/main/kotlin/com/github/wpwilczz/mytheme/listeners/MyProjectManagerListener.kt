c1102e4ead1d6072bae7e148180de6d1ad7436fd
GitHub Action <action@github.com>
2022-09-02 13:27:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/wpwilczz/mytheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wpwilczz/mytheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wpwilczz.mytheme.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wpwilczz.mytheme.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/wpwilczz/mytheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wpwilczz/mytheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wpwilczz.mytheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wpwilczz.mytheme.MyBundle
 
 class MyApplicationService {
 
